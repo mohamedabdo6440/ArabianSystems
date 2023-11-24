fc04bf141a56c48746923a0ce78a38805314fbd9
Mohamed Abdo <mohamedabdo6440@gmail.com>
2023-11-24 16:57:13 +0200

    create  carDetails file

--- vehicles_list/src/pages/carDetails/[...slug].tsx
+++ vehicles_list/src/pages/carDetails/[...slug].tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
+import CardDetails from "@/components/CardUse/CardDetails";
 const index = () => {
   const router = useRouter();
   const { slug } = router.query;
-
+const []=useState()
   return (
     <div>
       <h1>Dynamic Page</h1>
       <p>Slug: {slug}</p>
-      
+      {/* <CardDetails /> */}
     </div>
   );
 };
